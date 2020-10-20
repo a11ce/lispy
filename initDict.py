5eb10a9bd926fd55e947ff82ec0f2d2d30746693
a11ce <a11ce@protonmail.com>
2020-10-19 21:09:54 -0400

    +, -, * builtins

---
+++ initDict.py
@@ -0,0 +1,10 @@
+symDict = {
+    '+': lambda *x: 0 if len(x) == 0 else (x[0] if len(x) == 1 else x[0] + symDict['+'](*x[1:])),
+    '-': lambda *x: -1 * x[0] if len(x) == 1 else x[0] - symDict['+'](*x[1:]),
+    '*': lambda *x: 1 if len(x) == 0 else (x[0] if len(x) == 1 else x[0] *  symDict['*'](*x[1:])),
+    #'/': lambda *x: x[0] if len(x) == 1 else x[0] / symDict['/'](*x[1:]),
+    #'/': lambda *x: division(*x),
+    #'%': lambda *x: sum(x),
+    #'**': lambda *x: sum(x),
+    #'//': lambda *x: sum(x),
+}
